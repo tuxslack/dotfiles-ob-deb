a070968f322038dc2aa5816dca8aebe749b18377
ob-deb <blau@babass.org>
2017-07-05 20:47:27 -0300

    Create autostart.sh

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,9 @@
+compton -cCGfFz -o 0.38 -O 200 -I 200 -t -23 -l -23 -r 16 -D 2 --shadow-exclude "class_g = 'Firefox' && argb" -b &
+nitrogen --restore &
+numlockx &
+tint2 &
+# goldendict &
+thunderbird &
+whatsie &
+orage &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
